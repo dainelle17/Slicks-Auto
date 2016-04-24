664839a86bad212a0ae5cc3fe0b45f1dbf57eb48
Dainelle Riley <dainellecriley@ufl.edu>
2016-04-24 03:45:12 -0400

    insert dummy text for about us

--- views/about.php
+++ views/about.php
@@ -1,1 +1,67 @@
-About
+<section class="about-us row">
+	<div class="about-item twelve columns">
+		<div class="one columns space"></div>
+		<img class="two columns" src="<?php bloginfo('url'); echo '/images/aboutus1.png'; ?>">
+		<div class="one columns space"></div>
+		<div class="eight columns space">
+			<h2>
+				Hi, My name is ___
+			</h2>
+			<p>
+				This is some info on me
+			</p>
+		</div>
+	</div>
+	<div class="about-item twelve columns">
+		<div class="one columns space"></div>
+		<img class="two columns" src="<?php bloginfo('url'); echo '/images/aboutus1.png'; ?>">
+		<div class="one columns space"></div>
+		<div class="eight columns space">
+			<h2>
+				Hi, My name is ___
+			</h2>
+			<p>
+				This is some info on me
+			</p>
+		</div>
+	</div>
+	<div class="about-item twelve columns">
+		<div class="one columns space"></div>
+		<img class="two columns" src="<?php bloginfo('url'); echo '/images/aboutus1.png'; ?>">
+		<div class="one columns space"></div>
+		<div class="eight columns space">
+			<h2>
+				Hi, My name is ___
+			</h2>
+			<p>
+				This is some info on me
+			</p>
+		</div>
+	</div>
+	<div class="about-item twelve columns">
+		<div class="one columns space"></div>
+		<img class="two columns" src="<?php bloginfo('url'); echo '/images/aboutus1.png'; ?>">
+		<div class="one columns space"></div>
+		<div class="eight columns space">
+			<h2>
+				Hi, My name is ___
+			</h2>
+			<p>
+				This is some info on me
+			</p>
+		</div>
+	</div>
+	<div class="about-item twelve columns">
+		<div class="one columns space"></div>
+		<img class="two columns" src="<?php bloginfo('url'); echo '/images/aboutus1.png'; ?>">
+		<div class="one columns space"></div>
+		<div class="eight columns space">
+			<h2>
+				Hi, My name is ___
+			</h2>
+			<p>
+				This is some info on me
+			</p>
+		</div>
+	</div>
+</section>
--- views/blog.php
+++ views/blog.php
@@ -1,2 +1,16 @@
-
-Blog
+<?php
+                    if ( have_posts() ) {
+                        while ( have_posts() ) {
+                            the_post(); ?>
+                            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                            <?php
+                            if ( has_post_thumbnail() ) {
+                                the_post_thumbnail('thumbnail');
+                            }
+                            ?>
+                            <?php the_excerpt();?>
+                            <a href="<?php the_permalink(); ?>">Read More...</a>
+                        <?php } // end while
+                    } // end if
+                    
+                ?>
