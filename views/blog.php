a45a8ea6ee49b67a8bfe2d330cbc00b5d5ece92f
Dainelle Riley <dainellecriley@ufl.edu>
2016-04-24 09:58:41 -0400

    add font styling to css

--- views/blog.php
+++ views/blog.php
@@ -1,16 +1,27 @@
-<?php
-                    if ( have_posts() ) {
-                        while ( have_posts() ) {
-                            the_post(); ?>
-                            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                            <?php
-                            if ( has_post_thumbnail() ) {
-                                the_post_thumbnail('thumbnail');
-                            }
-                            ?>
-                            <?php the_excerpt();?>
-                            <a href="<?php the_permalink(); ?>">Read More...</a>
-                        <?php } // end while
-                    } // end if
-                    
-                ?>
+<?php 
+/*Template Name: Blog Posts */
+get_header(); ?>
+<?php query_posts('post_type=post&post_status=publish&posts_per_page=10&paged='. get_query_var('paged')); ?>
+    <div class="blog-wrapper">
+    <div class="row">
+        <div class="twelve columns">
+            <?php if (have_posts()) : 
+                while (have_posts()) : the_post(); ?> 
+                    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+                    <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                    <?php the_excerpt(__('Continue reading »','example'));
+                endwhile; ?>
+                <!-- Navigation -->
+                <div class="navigation">
+                    <span class="newer"><?php previous_posts_link(__('« Newer','example')) ?></span> 
+                    <span class="older"><?php next_posts_link(__('Older »','example')) ?></span>
+                </div>
+            <?php else: ?>
+                <div class="404-section">
+                    <p><?php _e('None found.', 'example'); ?></p>
+                </div>
+            <?php endif; wp_reset_query(); ?>
+        </div>
+    </div>
+    </div>
+<?php get_footer(); ?>
